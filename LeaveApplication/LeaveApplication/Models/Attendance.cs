03d1e384a11b79f0b9decafaccb815cc4b453032
MUHAMMAD RAFAY <MUHAMMAD RAFAY@DESKTOP-6996G2B>
2019-11-06 21:48:43 +0500

    excel file read functions implemented

---
+++ LeaveApplication/LeaveApplication/Models/Attendance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Attendance
+    {
+        public int EmpId { get; set; }
+        public bool Abscent { get; set; }
+    }
+}
---
+++ LeaveApplication/LeaveApplication/Models/Excel.cs
@@ -0,0 +1,51 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Excels
+    {
+
+        public List<Attendance> Read(FileStream FileStream)
+        {
+           
+            List<Attendance> l1 = new List<Attendance>();
+
+            using (var excelWorkbook = new XLWorkbook(FileStream))
+            {
+                var nonEmptyDataRows = excelWorkbook.Worksheet(1).RowsUsed();
+
+                foreach (IXLRow dataRow in nonEmptyDataRows)
+                {
+                   
+                    if (dataRow.RowNumber() != 1)
+                    {
+                        var cell = dataRow.Cell(1).Value;
+                        var cell2 = dataRow.Cell(12).Value;
+                        l1.Add(new Attendance()
+                        {
+                            EmpId = Convert.ToInt32(cell.ToString()),
+                            Abscent = Convert.ToBoolean(cell2.ToString())
+
+                        });
+                        Console.WriteLine(cell.ToString() + "\t" + cell2);
+                    }
+
+
+
+                }
+            }
+            FileStream.Close();
+            return l1;
+
+        }
+
+
+    }
+}
