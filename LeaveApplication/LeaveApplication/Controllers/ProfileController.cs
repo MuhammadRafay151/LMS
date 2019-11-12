3b232f970518e5f03753445439d4e7141e2d8db8
Muhammad Aamir <Muhammad Aamir@DESKTOP-G11219I>
2019-11-12 21:08:43 +0500

    View Created

---
+++ LeaveApplication/LeaveApplication/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LeaveApplication.Models;
+using System.IO;
+using System.Data.SqlClient;
+
+
+namespace LeaveApplication.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        EmployeeBusinessLayer eb = new EmployeeBusinessLayer();
+        public ActionResult ResetPassword(string OldPassword,string NewPassword,string ComfirmPassword)
+        {
+            Employee e1 = (Employee)Session["Employee"];
+            if (Session["EmpID"] != null)
+            {
+                
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "LogIn");
+            }
+        }
+    }
+}
