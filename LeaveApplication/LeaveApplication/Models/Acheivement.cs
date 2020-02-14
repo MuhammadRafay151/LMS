8e31e13e6800051b6dea037b71cd0abfbda2f0e7
MUHAMMAD RAFAY <MUHAMMAD RAFAY@DESKTOP-6996G2B>
2020-02-15 00:59:53 +0500

    Acheivement ui added

---
+++ LeaveApplication/LeaveApplication/Models/Acheivement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace LeaveApplication.Models
+{
+    public class Acheivement
+    {
+        public int AcheivementId { get; set; }
+        [Required]
+        public string title { get; set; }
+        public string Description { get; set; }
+        public DateTime AcheivementDate { get; set; }
+    }
+}
