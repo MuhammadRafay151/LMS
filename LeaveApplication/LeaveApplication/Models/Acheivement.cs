a1f790e7ad435e91ae25a04b60f1c234799bce07
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-02-18 13:23:26 +0500

    Sql Connection Changed

--- LeaveApplication/LeaveApplication/Models/Acheivement.cs
+++ LeaveApplication/LeaveApplication/Models/Acheivement.cs
@@ -4,40 +4,50 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+
 namespace LeaveApplication.Models
 {
     public class Acheivement
     {
         public int AcheivementId { get; set; }
+        public int AcheivementAttachmentsID { get; set; }
         public int FileId { get; set; }
+
         [Required]
         public string title { get; set; }
+
         [Required]
         public string Description { get; set; }
+
         [Required]
         public DateTime AcheivementDate { get; set; }
+
         [Required]
         public HttpPostedFileBase File { get; set; }
+
         public string FileName { get; set; }
         public byte[] FileBytes { get; set; }
 
+        private db database = new db();
+        private string Querry;
 
-        db database = new db();
-        string Querry;
         public DataSet GetAcheivement()
         {
             Querry = "";
             return database.Read(Querry);
         }
+
         public void UpdateAcheivement()
         {
             Querry = "";
         }
+
         public void InsertAcheivement()
         {
             Querry = "";
             database.ExecuteQuerry(Querry);
         }
+
         public void DeleteAcheivement()
         {
             Querry = "";
