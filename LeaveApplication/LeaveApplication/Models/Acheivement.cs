33612a88fe79c620b1d0a6aca06bb8065a5c47e5
MUHAMMAD RAFAY <MUHAMMAD RAFAY@DESKTOP-6996G2B>
2020-02-18 13:16:21 +0500

    model changes

--- LeaveApplication/LeaveApplication/Models/Acheivement.cs
+++ LeaveApplication/LeaveApplication/Models/Acheivement.cs
@@ -9,10 +9,19 @@ namespace LeaveApplication.Models
     public class Acheivement
     {
         public int AcheivementId { get; set; }
+        public int FileId { get; set; }
         [Required]
         public string title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public DateTime AcheivementDate { get; set; }
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+        public string FileName { get; set; }
+        public byte[] FileBytes { get; set; }
+
+
         db database = new db();
         string Querry;
         public DataSet GetAcheivement()
