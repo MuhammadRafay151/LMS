3c6efa8cf9360e0586874225515ed5c760f94992
MUHAMMAD RAFAY <MUHAMMAD RAFAY@DESKTOP-6996G2B>
2020-02-18 12:28:33 +0500

    add attachment

---
+++ LeaveApplication/LeaveApplication/Controllers/AcheivementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaveApplication.Controllers
+{
+    public class AcheivementController : Controller
+    {
+        // GET: Acheivement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
