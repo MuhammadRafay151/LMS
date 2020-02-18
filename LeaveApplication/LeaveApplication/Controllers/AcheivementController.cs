00dcc10302d3a9a3edfdd585c63e50d47a336adc
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-02-18 22:11:52 +0500

    AddAcheviment Method Implemented

--- LeaveApplication/LeaveApplication/Controllers/AcheivementController.cs
+++ LeaveApplication/LeaveApplication/Controllers/AcheivementController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeaveApplication.Models;
 
 namespace LeaveApplication.Controllers
 {
     public class AcheivementController : Controller
     {
+        private Acheivement ac = new Acheivement();
+
         // GET: Acheivement
         public ActionResult Index()
         {
-            return View();
+            if (Session["EmpID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "LogIn");
+            }
+        }
+
+        public ActionResult AddAcheivement(Acheivement ach)
+        {
+            Employee e1 = (Employee)Session["Employee"];
+            if (Session["EmpID"] != null)
+            {
+                ac.InsertAcheivement(e1.EmployeeID, ach);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index", "LogIn");
+            }
         }
     }
 }
--- LeaveApplication/LeaveApplication/Models/Acheivement.cs
+++ LeaveApplication/LeaveApplication/Models/Acheivement.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace LeaveApplication.Models
 {
@@ -31,9 +33,9 @@ namespace LeaveApplication.Models
         private db database = new db();
         private string Querry;
 
-        public DataSet GetAcheivement()
+        public DataSet GetAcheivement(int EmployeeID)
         {
-            Querry = "";
+            Querry = string.Format("select Acheivement.id as 'AcheivementID',AcheivementAttachments.FileId as 'FileID',AcheivementAttachments.id as 'AcheivementAttachID' ,Title,Date,Description,FileName,Content from Acheivement inner join AcheivementAttachments on Acheivement.id=AcheivementId inner join Files on Files.FileId=AcheivementAttachments.FileId where EmployeeID={0}", EmployeeID);
             return database.Read(Querry);
         }
 
@@ -42,10 +44,30 @@ namespace LeaveApplication.Models
             Querry = "";
         }
 
-        public void InsertAcheivement()
+        public void InsertAcheivement(int EmployeeID, Acheivement ach)
         {
-            Querry = "";
-            database.ExecuteQuerry(Querry);
+            List<SqlParameter> sqlParameters = new List<SqlParameter>();
+            sqlParameters.Add(new SqlParameter() { ParameterName = "title", Value = ach.title });
+            sqlParameters.Add(new SqlParameter() { ParameterName = "AcheivementDate", Value = ach.AcheivementDate });
+            sqlParameters.Add(new SqlParameter() { ParameterName = "Description", Value = ach.Description });
+            sqlParameters.Add(new SqlParameter() { ParameterName = "EmployeeID", Value = EmployeeID });
+
+            Querry = string.Format(@"DECLARE @achid AS INT, @fileid int
+            insert into Acheivement(Title,Date,Description,EmployeeID) values(@title,@AcheivementDate,@Description,@EmployeeID);
+            set @achid = SCOPE_IDENTITY();
+
+            Insert into Files(Content) values(@content);
+            set @fileid = SCOPE_IDENTITY();
+
+            Insert into AcheivementAttachments(FileId,AcheivementId,FileName) values(@fileid,@achid,@FileName);
+            ");
+            sqlParameters.Add(new SqlParameter() { ParameterName = "FileName", Value = ach.FileName });
+            Stream s1 = ach.File.InputStream;
+            BinaryReader b1 = new BinaryReader(s1);
+            FileBytes = b1.ReadBytes((int)s1.Length);
+            sqlParameters.Add(new SqlParameter() { ParameterName = "content", Value = FileBytes });
+
+            database.ExecuteQuerry(Querry, sqlParameters);
         }
 
         public void DeleteAcheivement()
