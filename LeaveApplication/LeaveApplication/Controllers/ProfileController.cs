7f7879f0a5fa9de104cf2386abf1c92a25bc63d1
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-02-24 17:00:46 +0500

    Eduaction Modified

--- LeaveApplication/LeaveApplication/Controllers/ProfileController.cs
+++ LeaveApplication/LeaveApplication/Controllers/ProfileController.cs
@@ -15,8 +15,6 @@ namespace LeaveApplication.Controllers
         // GET: Profile
         private EmployeeBusinessLayer eb = new EmployeeBusinessLayer();
 
-        private Education edu = new Education();
-
         public ActionResult ResetPassword()
         {
             Employee e1 = (Employee)Session["Employee"];
@@ -80,77 +78,6 @@ namespace LeaveApplication.Controllers
             {
             }
             return RedirectToAction("Basicinfo");
-        }
-
-        public ActionResult Education()
-        {
-            Employee e1 = (Employee)Session["Employee"];
-            if (Session["EmpID"] != null)
-            {
-                ViewBag.List = edu.GetDegrees();
-                System.Data.DataSet x = edu.GetEducation(e1.EmployeeID);
-
-                return View(x);
-            }
-            else
-            {
-                return RedirectToAction("Index", "LogIn");
-            }
-        }
-
-        [HttpPost]
-        public ActionResult AddEducation(Education eu)
-        {
-            Employee e1 = (Employee)Session["Employee"];
-            if (Session["EmpID"] != null)
-            {
-                edu.AddEducation(eu, e1.EmployeeID);
-                return RedirectToAction("Education", "Profile");
-            }
-            else
-            {
-                return RedirectToAction("Index", "LogIn");
-            }
-        }
-
-        [HttpPost]
-        public ActionResult DeleteEducation(int EduID)
-        {
-            Employee e1 = (Employee)Session["Employee"];
-            if (Session["EmpID"] != null)
-            {
-                edu.DeleteEducation(e1.EmployeeID, EduID);
-                return RedirectToAction("Education", "Profile");
-            }
-            else
-            {
-                return RedirectToAction("Index", "LogIn");
-            }
-        }
-
-        [HttpPost]
-        public ActionResult UpdateEducation(Education eu)
-        {
-            Employee e1 = (Employee)Session["Employee"];
-            if (Session["EmpID"] != null)
-            {
-                edu.UpdateEducation(eu, e1.EmployeeID);
-                return RedirectToAction("Education", "Profile");
-            }
-            else
-            {
-                return RedirectToAction("Index", "LogIn");
-            }
-        }
-
-        public JsonResult getedu(int? EduID)
-        {
-            Employee e1 = (Employee)Session["Employee"];
-            System.Data.DataSet x = edu.GetEdu(e1.EmployeeID, Convert.ToInt32(EduID));
-            //x.Tables[0].
-            string z = JsonConvert.SerializeObject(x);
-
-            return Json(z, JsonRequestBehavior.AllowGet);
         }
     }
 }
